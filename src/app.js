fe0b2059ce6616bacc51c6bff7e20020cfed9175
Anurag <anuragkamboj20@gmail.com>
2025-02-18 11:02:38 +0530

    allow credentials from frontend

--- src/app.js
+++ src/app.js
@@ -1,23 +1,21 @@
 require("dotenv").config();
 //change
-const express=require('express');
+const express = require("express");
 const app = express();
-const cors=require('cors');
+const cors = require("cors");
 const { eventcardRouter } = require("../api/v1/router/eventcardRouter.js");
-require('../config/db.js');
+require("../config/db.js");
 // const {todoRouter}=require('../api/v1/router/todoRouter.js');
 // console.log(Todo);
 
-const PORT=process.env.port||1900;
+const PORT = process.env.port || 1900;
 
-app.use(cors({origin: "http://localhost:5173"}));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json()); // body parser middleware
 
 // it handles request related to TODOs
-app.use('/api/v1/eventcard',eventcardRouter);
+app.use("/api/v1/eventcard", eventcardRouter);
 
-
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}---------`)
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}---------`);
 });
-
