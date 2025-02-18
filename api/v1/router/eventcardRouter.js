e07e4f0bbc09c9213d8d6acb592b25358750d551
Anurag <anuragkamboj20@gmail.com>
2025-02-18 12:21:54 +0530

    test

--- api/v1/router/eventcardRouter.js
+++ api/v1/router/eventcardRouter.js
@@ -1,5 +1,5 @@
 const express=require('express');
-const { listeventcards} = require('../controllers/eventCardControlle.js');
+const { listeventcards} = require('../controllers/eventcardController.js');
 const eventcardRouter=express.Router();
 eventcardRouter.get('/',listeventcards);  
 module.exports={eventcardRouter};
